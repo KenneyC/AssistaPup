f64d5d5d126c3431f387d72e3c1f3e45e1daf7a5
KenneyC <Kenney.CHN@gmail.com>
2020-12-07 09:53:15 +1300

    Remove comments

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,10 @@ import { Telegraf, session, Stage } from 'telegraf';
 import * as dotenv from 'dotenv';
 import express from 'express';
 
-import { newAgendaMeetingType, newAgendaDate } from './scenarios/new-agenda';
-import { askForAgendaScene } from './scenarios/ask-for-agenda';
+import { newAgendaMeetingType, newAgendaDate, NEW_AGENDA_MEETING_TYPE, generateMinutesTemplate } from './scenarios/new-agenda';
+import { askForAgendaScene, ASK_FOR_AGENDA_ID } from './scenarios/ask-for-agenda';
 import connection from './utils/connections/database';
+import { subscribeCommand } from './scenarios/ask-for-subs';
 
 dotenv.config();
 const app = express();
@@ -38,17 +39,17 @@ bot.start(async (ctx) => {
 bot.use(session());
 bot.use(stage.middleware());
 
-// bot.command('askforsubs', subscribeCommand );
-// bot.command('newagenda', (ctx: any) => {
-// 	(ctx as any).session.agendaSetUp = {
-// 		meetingType: "",
-// 		meetingDate: "",
-// 	}
-// 	return ctx.scene.enter(NEW_AGENDA_MEETING_TYPE)
-// });
+bot.command('askforsubs', subscribeCommand );
+bot.command('newagenda', (ctx: any) => {
+	(ctx as any).session.agendaSetUp = {
+		meetingType: "",
+		meetingDate: "",
+	}
+	return ctx.scene.enter(NEW_AGENDA_MEETING_TYPE)
+});
 
-// bot.action('start_agenda_collection', (ctx: any) => ctx.scene.enter(ASK_FOR_AGENDA_ID));
-// bot.action('generate_minutes_template', generateMinutesTemplate)
+bot.action('start_agenda_collection', (ctx: any) => ctx.scene.enter(ASK_FOR_AGENDA_ID));
+bot.action('generate_minutes_template', generateMinutesTemplate)
 
 bot.launch(); // start
 
